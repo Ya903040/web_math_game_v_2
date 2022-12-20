77545d67f8b7e6c1b3588a36b23584a996dab5c6
asiro <asirov560@gmail.com>
2022-12-20 21:03:09 +0600

    trying to add timer

--- bundle.js
+++ bundle.js
@@ -9,19 +9,7 @@ console.log(myCoolDate);
 console.log('dауауаf');
 
 
-    let countdown = 10;
-    let intervalID = setInterval(function () {
-        // Decrement the countdown
-        countdown--;
 
-        // Update the countdown on the page
-        document.getElementById("timer").innerHTML = countdown;
-
-        // If the countdown reaches zero, redirect the page
-        if (countdown === 0) {
-            location.href = "time_is_up.html";
-        }
-    }, 1000);
 },{"moment":2}],2:[function(require,module,exports){
 //! moment.js
 //! version : 2.29.4
